805435b2b65f4464a11d9cd6d94e4faa00b1099d
Daniel Guaman <dmguaman@gmail.com>
2018-11-16 21:13:06 -0500

    Add read paper

--- TESIS002/Controllers/PacienteController.cs
+++ TESIS002/Controllers/PacienteController.cs
@@ -15,13 +15,13 @@ namespace TESIS002.Controllers
         {
 			var ListaPaciente = new List<Paciente>
 			{
-				new Paciente(){ IdPaciente =1, NombrePaciente="JUAN", CedulaPaciente =1111111111, HistoriaClinicaPaciente =99999999, },
+				/*new Paciente(){ IdPaciente =1, NombrePaciente="JUAN", CedulaPaciente =1111111111, HistoriaClinicaPaciente =99999999, },
 				new Paciente(){ IdPaciente =1, NombrePaciente="PEPE", CedulaPaciente =1111111112, HistoriaClinicaPaciente =99999990, },
 				new Paciente(){ IdPaciente =1, NombrePaciente="ROSA", CedulaPaciente =1111111113, HistoriaClinicaPaciente =99999991, },
-				new Paciente(){ IdPaciente =1, NombrePaciente="LUCIA",CedulaPaciente =1111111114, HistoriaClinicaPaciente =99999992,}
+				new Paciente(){ IdPaciente =1, NombrePaciente="LUCIA",CedulaPaciente =1111111114, HistoriaClinicaPaciente =99999992,}*/
 			};
 
-			var resultado = ListaPaciente.Where(X => X.NombrePaciente == nombrePaciente).FirstOrDefault();
+            var resultado = "";//ListaPaciente.Where(X => X.NombrePaciente == nombrePaciente).FirstOrDefault();
 			return View(resultado);
 		}
         public ActionResult Ingresar()
