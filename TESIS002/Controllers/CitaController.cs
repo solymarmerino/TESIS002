0e65eb9b1dc86a42adbde495b52231cc64797f5e
Daniel Guaman <dmguaman@gmail.com>
2018-12-03 18:54:21 -0500

    Add cita

--- TESIS002/Controllers/CitaController.cs
+++ TESIS002/Controllers/CitaController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TESIS002.Models;
+using TESIS002.Object;
 
 namespace TESIS002.Controllers
 {
     public class CitaController : Controller
     {
+        ListasPaciente listasPaciente = new ListasPaciente();
+        ListasCitas listasCita = new ListasCitas();
         // GET: Cita
+        [HttpGet]
         public ActionResult Ingresar()
         {
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ingresar(CitaModel cita)
+        {
+            if (!string.IsNullOrEmpty(cita.IdPaciente) && !string.IsNullOrEmpty(cita.Especialidad) && !string.IsNullOrEmpty(cita.Medico) && !string.IsNullOrEmpty(cita.TipoCita))
+            {
+                int numeroCita = listasCita.numberOfCita() + 1;
+                cita.IdCita = numeroCita.ToString();
+                listasCita.addCita(cita);
+            }
+            CitaModel citaEnviar = cita;
+            citaEnviar.Paciente = listasPaciente.searchPaciente(cita.IdPaciente);
+            citaEnviar.ListaCita = listasCita.getListaCitaPaciente(cita.IdPaciente);
+            return View(citaEnviar);
         }
 
         public ActionResult Listar()
---
+++ TESIS002/Object/ListasCita.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using TESIS002.Models;
+
+namespace TESIS002.Object
+{
+	public class ListasCitas
+	{
+		List<CitaModel> listaCita = new List<CitaModel>();
+
+		public ListasCitas()
+		{
+
+		}
+
+		public void addCita(CitaModel cita)
+		{
+			string rutaCompleta = @"E:\Cita.txt";
+
+			using (StreamWriter file = new StreamWriter(rutaCompleta, true))
+			{
+				string texto = $"{cita.IdCita};" +
+							   $"{cita.IdPaciente};" +
+							   $"{cita.Especialidad};" +
+				               $"{cita.Medico};" +
+							   $"{cita.TipoCita}";
+				file.WriteLine(texto);
+				file.Close();
+			}
+		}
+
+		public List<CitaModel> getListaCita()
+		{
+			string rutacompleta = @"E:\Cita.txt";
+			string[] renglones = File.ReadAllLines(rutacompleta);
+
+			try
+			{
+				foreach (var linea in renglones)
+				{
+					CitaModel cita = new CitaModel();
+					string[] datos = linea.Split(';');
+					cita.IdCita = datos[0];
+					cita.IdPaciente = datos[1];
+					cita.Especialidad = datos[2];
+					cita.Medico = datos[3];
+					cita.TipoCita = datos[4];
+					
+					this.listaCita.Add(cita);
+
+				}
+				return this.listaCita;
+			}
+
+			catch (Exception e)
+			{
+				return this.listaCita;
+			}
+		}
+
+        public List<CitaListaModel> getListaCitaPaciente(string idPaciente)
+        {
+            List<CitaListaModel> citasPaciente = new List<CitaListaModel>();
+            this.listaCita = this.getListaCita();
+
+            foreach (var cita in listaCita)
+            {
+                if (cita.IdPaciente.Equals(idPaciente))
+                {
+                    CitaListaModel citaLista = new CitaListaModel();
+                    citaLista.IdCita = cita.IdCita;
+                    citaLista.IdPaciente = cita.IdPaciente;
+                    citaLista.Especialidad = cita.Especialidad;
+                    citaLista.Medico = cita.Medico;
+                    citaLista.TipoCita = cita.TipoCita;
+
+                    citasPaciente.Add(citaLista);
+                }
+            }
+            return citasPaciente;
+        }
+
+
+        public int numberOfCita()
+		{
+			this.listaCita = this.getListaCita();
+			int numero = this.listaCita.Count() - 1;
+			int siguienteId = 0;
+            if(numero >= 0)
+            {
+                siguienteId = Int32.Parse(this.listaCita[numero].IdCita);
+            }            
+            return siguienteId;
+		}
+
+	}
+}
